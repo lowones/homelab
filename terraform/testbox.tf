1a87440f7c6ca638597503e4417cb3ef9353c121
lowone <lowones@gmail.com>
2026-04-11 23:18:33 -0400

    use vim instead of nano

---
+++ terraform/testbox.tf
@@ -0,0 +1,47 @@
+resource "proxmox_virtual_environment_vm" "testbox" {
+  name      = "testbox"
+  node_name = "grid"
+  vm_id     = 220
+
+  agent {
+    enabled = false
+  }
+
+  clone {
+    vm_id = 9000
+    full  = true
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 20
+  }
+
+  network_device {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "192.168.0.220/24"
+        gateway = "192.168.0.1"
+      }
+    }
+    user_account {
+      username = "ubuntu"
+      keys     = [local.ssh_public_key]
+    }
+  }
+}
